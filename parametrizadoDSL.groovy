84ec5babf10fec617b459bdfffec936efffc7d56
sergiosuarezv <153043703+sergiosuarezv@users.noreply.github.com>
2024-03-22 13:39:27 -0600

    Cambios de nombre

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -2,12 +2,12 @@ job('ejemplo2-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
       		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+        		node / gitConfigName('sergiosuarezv')
+        		node / gitConfigEmail('sergiosuarez.v@gmail.com')
       		}
     	} 
   	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
+   		stringParam('nombre', defaultValue = 'Sergio', description = 'Parametro de cadena para el Job Booleano')
       		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
       		booleanParam('agente', false)
     	}
@@ -18,23 +18,7 @@ job('ejemplo2-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
+      		mailer('sergiosuarez.v@gmail.com', true, true)
+
     	}
 }
--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -2,8 +2,8 @@ job('ejemplo3-job-DSL') {
 	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
       		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
+        		node / gitConfigName('sergiosuarezv')
+        		node / gitConfigEmail('sergiosuarez.v@gmail.com')
       		}
     	} 
   	parameters {
@@ -19,23 +19,6 @@ job('ejemplo3-job-DSL') {
     		shell("bash jobscript.sh")
     	}
   	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
+      		mailer('sergiosuarez.v@gmail.com', true, true)
     	}
 }
